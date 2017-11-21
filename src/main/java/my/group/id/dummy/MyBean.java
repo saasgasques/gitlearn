ec9f647a6896d16a8fee495d133ec1b919be26f7
ALTEN <ALTEN@SG-713.prod.altengroup.dir>
2017-11-21 17:26:02 +0100

    Commitejar desde l'eclipse

---
+++ src/main/java/my/group/id/dummy/MyBean.java
@@ -0,0 +1,16 @@
+package my.group.id.dummy;
+
+public class MyBean {
+
+	private String greeting;
+
+	public String getGreeting() {
+		return greeting;
+	}
+
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+	
+	
+}
