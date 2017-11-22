12e1af7728b2874887e34c1ef19673ef35784e47
carles.gasques@gmail.com <Berenguer2012>
2017-11-22 13:20:55 +0100

    Afegir un comentari local

--- src/main/java/my/group/id/dummy/App.java
+++ src/main/java/my/group/id/dummy/App.java
@@ -4,8 +4,8 @@ package my.group.id.dummy;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
+	// Comentari local
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
