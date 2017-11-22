a46e22e6ab1bb81dbd9776a7e3fec1485728760c
carles.gasques@gmail.com <Berenguer2012>
2017-11-22 13:11:20 +0100

    Afegir un mètode no estàtic

--- src/main/java/my/group/id/dummy/Add.java
+++ src/main/java/my/group/id/dummy/Add.java
@@ -4,5 +4,15 @@ public class Add {
   
   public static int add(int a, int b) {
     return a+b;
-  }  
+  }
+
+  public Integer addition(Integer a, Integer b) {
+      return Add.add(a, b);
+  }
+
+  public static void main(String[] args) {
+      Add add = new Add();
+      System.out.println(add.addition(3,2));
+      System.out.println(add(3,2));
+  }
 }
